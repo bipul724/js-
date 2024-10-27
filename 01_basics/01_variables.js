0858181157f02d6f75a658ed7fc5f040e388731f
bipul724 <bipulchamoli45@gmail.com>
2024-10-27 07:45:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "bipul@google.com"
+var accountPassword = "12345"
+accountCity = "Dun"
+
+// accountId = 2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
