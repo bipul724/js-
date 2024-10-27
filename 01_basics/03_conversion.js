a9f93970ab35246d4063aade2a2cb649d3726042
bipul724 <bipulchamoli45@gmail.com>
2024-10-27 08:23:37 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = 87;
+console.log(score);
+
+let score1 = "98abc"
+let valueInNumber = Number(score1);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let value ;
+let valueInBoolean = Boolean(value);
+console.log(valueInBoolean);
