a4dd95c82f68e8e6632a3692b7d1e5b5a14f1f82
bipul724 <bipulchamoli45@gmail.com>
2024-10-27 13:00:13 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,9 @@
+const myFunction = function(){
+    console.log("Hello World !!");
+
+}
+console.log(typeof(myFunction));
+
+const heros = ["superman","batman","spiderman"];
+console.log(typeof(heros));
+console.log(Array.isArray(heros));
