a990e1d2174ac1a2f0e857d209ef1bfb842ec42a
bipul724 <bipulchamoli45@gmail.com>
2024-10-29 11:54:48 +0000

    forof loop in array and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,28 @@
+const arr = [1,2,3,4,5];
+
+for (const num of arr) {
+    //console.log(num);
+    
+    
+}
+const greetings = "Hello world!";
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`);
+    
+    
+}
+
+const map = new Map();
+
+map.set('In','INDIA');
+map.set('USA','United states of america');
+map.set('Fr','France');
+map.set('In','INDIA');
+
+
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log(value);
+    
+}
