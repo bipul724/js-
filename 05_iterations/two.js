455e13468f10ca0cbdbc2b86603f1fe48f6b3e12
bipul724 <bipulchamoli45@gmail.com>
2024-10-29 11:54:20 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let i = 0;
+while (i<=10) {
+    console.log(i);
+    i++;
+    
+}
+
+let myArray = ["flash","batman","superman"];
+let j =0;
+while(j<myArray.length){
+    console.log(`value is ${myArray[j]}`);
+    j++;
+    
+}
