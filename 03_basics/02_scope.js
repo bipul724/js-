ee1f6532d3fb22defdee3e23b95e1cf5c9342a36
bipul724 <bipulchamoli45@gmail.com>
2024-10-29 05:29:58 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,50 @@
+if(true){
+    let a = 10;
+const b = 20;
+var c = 30;
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+function one(){
+    const username = "bipul"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+   // console.log(website);
+
+    two();
+}
+
+one();
+
+if(true){
+    const username = "bipul";
+    if(username=="bipul"){
+        const website = " youtube"
+        //console.log(username+website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
